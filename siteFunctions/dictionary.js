404e8da13425bfceab6363514f58d187fa01def8
João Gabriel Vieira Silva <143756812+Osoapy@users.noreply.github.com>
2024-03-24 14:47:31 +0000

    Upgrading html with majority of what's going to be necessary, making dictionary.js an exporting module, making buttons more appealable with css, removing an unnecessary '\n' in the README file

--- siteFunctions/dictionary.js
+++ siteFunctions/dictionary.js
@@ -1,1 +1,6 @@
-let palavras = ["Tapu", "Kaka", "Inque", "Crati", "Tala", "Lele", "Beg"];
+const palavras = ["Tapu", "Kaka", "Inque", "Crati", "Tala", "Lele", "Beg"];
+export default palavras;
+
+export function ping() {
+  console.log("Heya, I'm being imported from dictionary.js");
+}
--- siteFunctions/script.js
+++ siteFunctions/script.js
@@ -1,3 +1,8 @@
+// Imports
+import * as dictionary from './dictionary.js';
+
+dictionary.ping(); // Heya, .log() here!
+
 function constructPage() {
     let body = document.getElementById("keyboard");
     for(let k = 0; k < 26; k++) {
@@ -6,7 +11,7 @@ function constructPage() {
         button.classList = "letterButton";
         button.textContent = String.fromCharCode(65 + k);
         body.appendChild(button);
-        console.log("constructPage \"for\" interactioctionary.jsn number " + (k + 1)); // Heya, .log() here!
+        console.log("constructPage()\'s \"for\" interaction number " + (k + 1)); // Heya, .log() here!
     }
 }
 
